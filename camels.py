c2a2b11db8b4150792d2d4a1b1343ff46ba709cf
Victor Ananyev <vindex10@gmail.com>
2021-03-06 21:41:19 +0100

    pretty python. untested yet

--- camels.py
+++ camels.py
@@ -8,17 +8,17 @@ class Game:
     def __init__(self, camels=None, init_state=None):
         _camels = self._infer_camels(camels, init_state)
         self.die = CamelDie(_camels)
-        _state = self._infer_state(_camels, init_state)
+        _state = self._infer_state(self.die, init_state)
         self.board = Board(_state)
         if VERBOSE:
             self.board.show()
 
     def one_step(self):
-        camel, steps = self.die.roll()
+        camel, steps = self.die.roll_once()
         self.board.move_camel(camel, steps)
 
     def finish_round(self):
-        for camel, steps in self.die.finish_round():
+        for camel, steps in self.die.roll_finish_round():
             self.board.move_camel(camel, steps)
 
     def _infer_camels(self, camels, state):
@@ -31,40 +31,45 @@ class Game:
         return [camel for stack in state.pos_to_stack.values()
                 for camel in stack.camels]
 
-    def _infer_state(self, camels, state):
+    @classmethod
+    def _infer_state(cls, die, state):
         if state:
-            return state
-        return self._produce_init_state()
+            return state.copy()
+        return cls._produce_init_state(die)
 
-    def _produce_init_state(self):
+    @staticmethod
+    def _produce_init_state(die):
         pos_to_stack = {}
-        for camel, pos in self.die.finish_round():
+        for camel, pos in die.roll_finish_round():
             pos_to_stack.setdefault(pos, CamelStack([])).attach(CamelStack([camel]))
         return State(pos_to_stack)
 
 
 class CamelDie:
-    def __init__(self, camels, state=None):
+    MIN_STEPS = 1
+    MAX_STEPS = 3
+
+    def __init__(self, camels, die_state=None):
         self._camels = list(camels)
-        self.state = list(state) if state is not None else None
-        if state is None:
+        self.state = list(die_state) if die_state is not None else None
+        if die_state is None:
             self._reset_state()
 
-    def roll(self):
+    def roll_once(self):
         try:
             camel = self.state.pop()
         except IndexError:
             self._reset_state()
             camel = self.state.pop()
 
-        steps = randint(1, 3)
+        steps = randint(self.MIN_STEPS, self.MAX_STEPS)
         return camel, steps
 
-    def finish_round(self):
+    def roll_finish_round(self):
         res = []
         while self.state:
             camel = self.state.pop()
-            steps = randint(1, 3)
+            steps = randint(self.MIN_STEPS, self.MAX_STEPS)
             res.append((camel, steps))
         self._reset_state()
         return res
@@ -76,28 +81,67 @@ class CamelDie:
 
 class Board:
     def __init__(self, init_state):
-        self.set_state(init_state)
-
-    def set_state(self, camel_state):
-        self._camel_to_pos = self._index_camel_stacks(camel_state)
-        self.state = camel_state.copy()
+        self.state = MoveableState().consume(init_state.copy())
 
     def show(self):
-        sorted_pos = sorted(self.state.pos_to_stack)
+        sorted_pos = sorted(self.state.state.pos_to_stack)
         for pos in sorted_pos:
-            stack = self.state.pos_to_stack[pos]
+            stack = self.state.state.pos_to_stack[pos]
             print(pos, ":", stack.camels)
-        print(self._camel_to_pos)
+        print(self.state._camel_to_pos)
 
     def move_camel(self, camel, steps):
         if VERBOSE:
             print(camel, steps)
             self.show()
-        curpos = self._camel_to_pos[camel]
+        curpos = self.state.get_camel_pos(camel)
         newpos = curpos + steps
-        self._move_camel_from_to(camel, curpos, newpos)
+        self.state.move_camel_from_to(camel, curpos, newpos)
 
-    def _move_camel_from_to(self, camel, pos, newpos):
+
+class State:
+    def __init__(self, pos_to_stack):
+        self.pos_to_stack = self._copy(pos_to_stack)
+
+    def get(self):
+        return self.pos_to_stack
+
+    def copy(self):
+        return State(self._copy(self.pos_to_stack))
+
+    @staticmethod
+    def _copy(pos_to_stack):
+        res = {}
+        for pos, stack in pos_to_stack.items():
+            res[pos] = stack.copy()
+        return res
+
+    @classmethod
+    def from_dict(cls, camel_dict):
+        res = {}
+        for pos, camels in camel_dict.items():
+            res[pos] = CamelStack(list(camels))
+        return State(res)
+
+
+class MoveableState:
+    def __init__(self):
+        # use consume() to init state. improves readability
+        self._camel_to_pos = None
+        self.state = None
+
+    def consume(self, ref_state):
+        self._camel_to_pos = self._index_camel_stacks(ref_state)
+        self.state = ref_state
+        return self
+
+    def get(self):
+        return self.state()
+
+    def get_camel_pos(self, camel):
+        return self._camel_to_pos[camel]
+
+    def move_camel_from_to(self, camel, pos, newpos):
         oldstack_moved = self._detach_camelstack(pos, camel)
         self._attach_camelstack(oldstack_moved, newpos)
         self._update_index(oldstack_moved, newpos)
@@ -152,38 +196,14 @@ class CamelStack:
         return not bool(self.camels)
 
 
-class State:
-    def __init__(self, pos_to_stack):
-        self.pos_to_stack = self._copy(pos_to_stack)
-
-    def copy(self):
-        return State(self._copy(self.pos_to_stack))
-
-    @staticmethod
-    def _copy(pos_to_stack):
-        res = {}
-        for pos, stack in pos_to_stack.items():
-            res[pos] = stack.copy()
-        return res
-
-    def leaderboard(self):
-        return self._leaderboard(self.pos_to_stack)
-
-    @staticmethod
-    def _leaderboard(pos_to_stack):
-        positions = sorted(pos_to_stack)
-        res = []
-        for pos in positions:
-            stack = pos_to_stack[pos]
-            res.extend(list(stack.camels))
-        return res
-
-    @classmethod
-    def from_dict(cls, camel_dict):
-        res = {}
-        for pos, camels in camel_dict.items():
-            res[pos] = CamelStack(list(camels))
-        return State(res)
+def flatten_state(state):
+    pos_to_stack = state.pos_to_stack
+    positions = sorted(pos_to_stack)
+    res = []
+    for pos in positions:
+        stack = pos_to_stack[pos]
+        res.extend(list(stack.camels))
+    return res
 
 
 if __name__ == "__main__":
